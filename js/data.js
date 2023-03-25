9095de8ae891e44154e3f7591e24c5da54a67d7e
AqibAurangzeb <aqibaurangzebdeveloper@gmail.com>
2023-03-25 04:26:37 +0000

    improvements to desktop view

--- js/data.js
+++ js/data.js
@@ -1,41 +1,41 @@
 export const posts = [
     {
-        createdDate: "July 23, 2022",
+        createdDate: "March 25, 2023",
         imgPath: "images/blog-one-image.png",
         imgAlt: "test",
         title: "Blog one",
         description: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
     },
     {
-        createdDate: "July 23, 2022",
+        createdDate: "March 25, 2023",
         imgPath: "images/blog-two-image.png",
         imgAlt: "test",
         title: "Blog two",
         description: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
     },
     {
-        createdDate: "July 23, 2022",
+        createdDate: "March 25, 2023",
         imgPath: "images/blog-three-image.png",
         imgAlt: "test",
         title: "Blog three",
         description: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
     },
     {
-        createdDate: "July 23, 2022",
+        createdDate: "March 25, 2023",
         imgPath: "images/blog-four-image.png",
         imgAlt: "test",
         title: "Blog four",
         description: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
     },
     {
-        createdDate: "July 23, 2022",
+        createdDate: "March 25, 2023",
         imgPath: "images/blog-five-image.png",
         imgAlt: "test",
         title: "Blog five",
         description: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
     },
     {
-        createdDate: "July 23, 2022",
+        createdDate: "March 25, 2023",
         imgPath: "images/blog-six-image.png",
         imgAlt: "test",
         title: "Blog six",
